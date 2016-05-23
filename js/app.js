f8d1ac70c46875ee155da6e480d550db2e4c8564
rajkumar.selvam <rajkumar.selvam@lge.com>
2016-05-23 16:15:19 +0530

    Updated the footer of main page. Now its in angular

--- js/app.js
+++ js/app.js
@@ -4,9 +4,19 @@ mApp.controller("ArticleController", function ($scope, $http) {
     $http.get('./data/main-articles.json').
     success(function (data) {
         $scope.articles = data.articles;
-        console.log($scope.articles);
     }).
     error(function (error) {
         console.log("Error reading main-articles.json: " + error);
+    });
+});
+
+mApp.controller("ProfilesController", function ($scope, $http) {
+    $http.get('./data/profiles.json').
+    success(function (data) {
+        $scope.profiles = data.profiles;
+        console.log($scope.profiles);
+    }).
+    error(function (error) {
+        console.log("Error reading profiles.json: " + error);
     });
 });
